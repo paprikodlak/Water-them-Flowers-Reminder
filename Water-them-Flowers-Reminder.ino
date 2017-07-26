4753be3dccc153a000a2d159597def7f8bb3989b
paprikodlak <vit@pawlik.in>
2017-07-26 21:23:59 +0200

    V0.1 sketch

---
+++ Water-them-Flowers-Reminder.ino
@@ -0,0 +1,23 @@
+const int ledPin =  13;    
+
+int ledState = HIGH;           
+long previousMillis = 0;        
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);      
+}
+ 
+void loop()
+{
+  unsigned long currentMillis = millis();
+  
+  digitalWrite(ledPin, ledState);
+  delay(1000);
+  ledState = LOW;
+  digitalWrite(ledPin, ledState);
+
+  if(currentMillis>23*3600*1000 ) {
+      ledState = HIGH;
+    digitalWrite(ledPin, ledState);
+  }
+}
